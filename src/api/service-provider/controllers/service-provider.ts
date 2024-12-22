8a2a8534c51f2033b7d5f48cf67278af1fb47e71
dhimas <dhimassatrio71@gmail.com>
2024-12-22 23:38:18 +0700

    first Commit

---
+++ src/api/service-provider/controllers/service-provider.ts
@@ -0,0 +1,7 @@
+/**
+ * service-provider controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::service-provider.service-provider');
---
+++ src/api/service-provider/routes/service-provider.ts
@@ -0,0 +1,7 @@
+/**
+ * service-provider router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::service-provider.service-provider');
